272f40c830021b5e78ac4903c6bd377ad50b9c41
leehokyeong <envylhk@naver.com>
2022-05-29 00:57:17 +0900

    환경세팅 완료

---
+++ src/test/java/com/envylhk/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envylhk.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
